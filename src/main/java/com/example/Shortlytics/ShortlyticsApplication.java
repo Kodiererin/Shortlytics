ad484d614ff627ff99b6dbe1ff8e91466dd83a30
Kodiererin <iamujjwal45@gmail.com>
2024-09-12 23:03:15 +0530

    Adding Initials

---
+++ src/main/java/com/example/Shortlytics/ShortlyticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Shortlytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortlyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortlyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Shortlytics/ShortlyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shortlytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
