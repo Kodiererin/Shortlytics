c5c7b6fa63532723d2199537f9f001bed0b418ec
Ujjwal <110911377+Kodiererin@users.noreply.github.com>
2024-12-17 18:49:53 +0530

    (Fix) : Fixed minor issues

--- src/main/java/com/example/Shortlytics/repository/urlRepository.java
+++ src/main/java/com/example/Shortlytics/repository/urlRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 
 public interface urlRepository extends MongoRepository<URL,String> {
+    List<URL> findByNewUrl(String fullShortUrl);
+
     List<URL> findURLByNewUrlEndingWithIgnoreCase(String newURL);
 
     List<URL> findByInitialUrlContainingIgnoreCase(String oldUrl);
--- src/main/java/com/example/Shortlytics/utils/Utility.java
+++ src/main/java/com/example/Shortlytics/utils/Utility.java
@@ -1,9 +1,12 @@
 package com.example.Shortlytics.utils;
 
+import com.example.Shortlytics.models.URL;
+
+import java.util.List;
+
 public interface Utility {
     // to Shorten the URL
     public String shortenURl(String oldUrl);
     // to get the current date and time
     public String getCurrentDateTime();
-
 }
